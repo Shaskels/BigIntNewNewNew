b4709607587574e6d99284988332c3cd7eb26c6e
Shaskels <zalyaeva-03@mail.ru>
2022-12-14 00:44:21 +0700

    Миксер и Revers

--- SoundProcessor/SoundProcessor/SoundProcessor.cpp
+++ SoundProcessor/SoundProcessor/SoundProcessor.cpp
@@ -9,8 +9,18 @@ int main(int argc, char* argv[])
     Input in;
     GetProgramOptions(argc, argv, in);
     AudioFile file1;
+    AudioFile file2;
     load(in.files[0], file1);
-    MuteTheIntervalFactory factory;
+    //load(in.files[1], file2);
+    /*MuteTheIntervalFactory factory;
+    Converter* con = factory.MakeConverter();
+    (*con).ChangeSamples(file1.samples, 5, 33);*/
+    /*MixerFactory factory;
+    Converter* con = factory.MakeConverter();
+    con->start2 = 7;
+    con->samples2 = file2.samples;
+    (*con).ChangeSamples(file1.samples, 5, 33);*/
+    ReversFactory factory;
     Converter* con = factory.MakeConverter();
     (*con).ChangeSamples(file1.samples, 5, 33);
     save("out.wav", file1);
--- SoundProcessor/SoundProcessor/Сonverter.h
+++ SoundProcessor/SoundProcessor/Сonverter.h
@@ -1,30 +1,51 @@
 #pragma once
 #include <vector>
 
-#define byteRate 44100
+#define sampleRate 44100
 
 class Converter {
 public:
+	int start2 = 0;
+	std::vector<int16_t> samples2;
 	virtual int ChangeSamples(std::vector<int16_t>&, int, int) = 0;
 };
 
 class MuteTheInterval: public Converter {
 	int ChangeSamples(std::vector<int16_t>& samples, int start, int end) {
-		int startIndex = start * byteRate;
-		int endIndex = end * byteRate;
-		for (int i = startIndex; i < endIndex; i++) {
+		int startIndex = start * sampleRate;
+		int endIndex = end * sampleRate;
+		for (int i = startIndex; i <= endIndex; i++) {
 			samples[i] = 0;
 		}
 		return 0;
 	}
 };
 class Mixer : public Converter {
-	int ChangeSamples(std::vector<int16_t>& samples, int start, int end) {
+
+	/*Mixer(int s, std::vector<int16_t> samp) {
+		start2 = s;
+		samples2 = samp;
+	}*/
+	int ChangeSamples(std::vector<int16_t>& samples1, int start1, int end1) {
+		int startIndex = start1 * sampleRate;
+		int endIndex = end1 * sampleRate;
+		int changeStart = start2 * sampleRate;
+		for (int i = startIndex; i <= endIndex; i++) {
+			samples1[i] = (samples1[i] + samples2[changeStart + i - startIndex]) / 2;
+		}
 		return 0;
 	}
 };
-class Replace : public Converter {
+class Revers : public Converter {
 	int ChangeSamples(std::vector<int16_t>& samples, int start, int end) {
+		int startIndex = start * sampleRate;
+		int endIndex = end * sampleRate;
+		int16_t tmp;
+		for (int i = startIndex; i < (endIndex + startIndex)/ 2; i++) {
+			tmp = samples[i];
+			samples[i] = samples[endIndex + startIndex - i];
+			samples[endIndex + startIndex - i] = tmp;
+		}
 		return 0;
 	}
 };
@@ -39,12 +60,14 @@ public:
 	}
 };
 class MixerFactory : public ConverterFactory {
+public:
 	Converter* MakeConverter() {
 		return new Mixer();
 	}
 };
-class ReplaceFactory : public ConverterFactory{
+class ReversFactory : public ConverterFactory{
+public:
 	Converter* MakeConverter() {
-		return new Replace();
+		return new Revers();
 	}
 };
